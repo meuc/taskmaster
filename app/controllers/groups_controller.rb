4b44ee668a18402fa39686aa38d96811e5e5e91f
Marie Crabit <mar.rie.cra@gmail.com>
2015-03-29 04:20:04 +0200

    creation of groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,13 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
+  
   def new
     @group = Group.new
+    
+    if current_user.group?
+      flash[:alert] = "You are already in a group"
+      redirect_to root_path
+    end
   end
   
   def create
@@ -20,8 +26,22 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
     
     if @group != current_user.group
-      flash[:error] = "You are not a member of this group"
+      flash[:alert] = "You are not a member of this group"
       redirect_to root_path
     end
   end
+  
+  def destroy
+    @group = Group.find(params[:id])
+    
+    @group.users.each do |user|
+      user.update group_id: nil
+    end
+    
+    @group.destroy
+    flash[:notice] = "Group has been deleted"
+    redirect_to root_path
+  end
 end
+
+# TODO: remove create group link on create group page or make less prominent
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :users 
-  resources :groups, only: [:new, :create, :show]
+  resources :groups, only: [:new, :create, :show, :destroy]
   # TODO: add edit, delete
   
   root to: "home#index"
