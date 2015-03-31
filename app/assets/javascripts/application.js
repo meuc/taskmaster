0e17b658de64217544720eb33f4f0e41c0dda04b
Marie Crabit <mar.rie.cra@gmail.com>
2015-03-31 22:49:10 +0200

    adding users to group

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,18 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require maxibear
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function(){
+	$(document).on("click", ".add-user-button", function(){
+		var last = $(".add-user-field").last();
+		var clone = last.clone();
+		clone.find("input").val("");
+		last.after(clone);
+	});
+	
+	$(document).on("click", ".add-user-field button", function(){
+		$(this).parents(".add-user-field").remove();
+	});
+});
